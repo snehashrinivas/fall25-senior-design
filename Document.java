83bcc346fdfb66a5b519a7cf23b78903b92b9455
andersen-breyel <andersenbreyel12329@gmail.com>
2025-11-21 11:55:25 -0600

    Add files via upload
    Rough document class

---
+++ Document.java
@@ -0,0 +1,42 @@
+package backend;
+
+// Class for interfacing with imported files
+// Written by Andersen Breyel
+public class Document {
+    private String fileName;
+    private int wordCount;
+    private int importDate;
+
+    public Document(String fileName, int wordCount, int importDate) {
+
+        this.fileName = fileName;
+        this.wordCount = wordCount;
+        this.importDate = importDate;
+    }
+
+    // Getters
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getWordCount(){
+        return wordCount;
+    }
+
+    public int getImportDate() {
+        return importDate;
+    }
+
+    // Setters
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setWordCount(int wordCount) {
+        this.wordCount = wordCount;
+    }
+
+    public void setImportDate(int importDate) {
+        this.importDate = importDate;
+    }
+}
